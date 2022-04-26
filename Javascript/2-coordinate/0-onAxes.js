c0a2e8b6756a9d9fcbfdf17a1edf21168199de85
Andresito <andrespodadera87.gmail.com>
2022-04-26 20:21:46 +0200

    actualizando 0-coordinate

--- Javascript/2-coordinate/0-onAxes.js
+++ Javascript/2-coordinate/0-onAxes.js
@@ -18,13 +18,26 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-let abscissa = console.readNumber(`Dame la abcisa de la coordenada:`);
-
-let ordered = console.readNumber(`Dame la ordenada de la coordenada:`);
-
-let axisXY =
-  abscissa == 0 && ordered == 0
-    ? "esta en el eje de abcisas y de ordenadas"
-    : "";
-let inOrdered = abscissa > 0 && ordered == 0 ? "esta en el eje de ordenadas"
-let inAbscissas = 
+const abciseCoordinate = console.readNumber(
+  `Dame la abcisa de la coordenada: `
+);
+const ordinateCoordinate = console.readNumber(
+  `Dame la ordenada de la coordenada: `
+);
+let msg = `La coordenada (${abciseCoordinate}, ${ordinateCoordinate})`;
+const msgAbcises = ` está en el eje de abcisas`;
+const msgOrdinates = ` está en el eje de ordenadas`;
+const msgTwoAxes =
+  abciseCoordinate === 0 && ordinateCoordinate === 0
+    ? `${msgAbcises} y de ordenadas`
+    : ``;
+const msgOneAxe =
+  msgTwoAxes === ``
+    ? abciseCoordinate === 0
+      ? msgAbcises
+      : ordinateCoordinate === 0
+      ? msgOrdinates
+      : ``
+    : msgTwoAxes;
+msg += msgOneAxe === `` ? ` no está en ningún eje` : msgOneAxe;
+console.writeln(msg);
