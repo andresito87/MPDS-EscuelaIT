5c0918dd8d478fed75a8d3d98bd726d8cd982eee
Andresito <andrespodadera87.gmail.com>
2022-04-27 21:53:02 +0200

    actualizando ejercicios JS

--- Javascript/3-date/2-previous.js
+++ Javascript/3-date/2-previous.js
@@ -1,3 +1,19 @@
+// const { Console } = require("console-mpds");
+// const console = new Console();
+
+// const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// const day = console.readNumber(`Dame el día: `);
+// const month = console.readNumber(`Dame el mes: `);
+// const year = console.readNumber(`Dame el año: `);
+
+// let monthDatePrevious = day === 1 ? month - 1 : month;
+// monthDatePrevious = monthDatePrevious == 0 ? 12 : monthDatePrevious;
+// let dayDatePrevious =  day === 1  ? DAYS_IN_MONTH[monthDatePrevious-1] : day - 1;
+// let yearDatePrevious = day === 1  && month === 1 ? year - 1 : year;
+
+// console.writeln(`La fecha ${day}/${month}/${year} y el anterior es ${dayDatePrevious}/${monthDatePrevious}/${yearDatePrevious}`);
+
 const { Console } = require("console-mpds");
 const console = new Console();
 
