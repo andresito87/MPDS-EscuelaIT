f7b3caece91321254335c232fa02e719a3ad28a8
Andresito <andrespodadera87.gmail.com>
2022-04-27 15:40:02 +0200

    2-previous terminado

--- Javascript/3-date/2-previous.js
+++ Javascript/3-date/2-previous.js
@@ -1,19 +1,3 @@
-// Dame el día: 12
-// Dame el mes: 12
-// Dame el año: 12
-
-// La fecha 12/12/12 y la siguiente es 11/12/12
-// Dame el día: 1
-// Dame el mes: 1
-// Dame el año: 12
-
-// La fecha 30/1/12 y la siguiente es 30/12/11
-// Dame el día: 1
-// Dame el mes: 4
-// Dame el año: 12
-
-// La fecha 1/4/12 y la siguiente es 30/3/12
-
 const { Console } = require("console-mpds");
 const console = new Console();
 
@@ -24,7 +8,7 @@ const anyo = console.readNumber(`Dame el año:`);
 console.write(
   `La fecha es ${dia}/${mes}/${anyo} y la anterior es ${
     dia === 1 ? 31 : dia - 1
-  }/${dia === 1 && mes === 1 ? 12 : mes - 1}/${
+  }/${dia === 1 && mes - 1 !== 0 ? mes - 1 : 12}/${
     dia === 1 && mes === 1 ? anyo - 1 : anyo
   } `
-); //hay que retocarlo
+);
