8e88d2cf6bf064f8d1df8edbfcdecd6169e46b2b
Andresito <andrespodadera87.gmail.com>
2022-04-27 23:56:00 +0200

    update scale and console.js parseFloat

--- Javascript/1-interval/2-scale.js
+++ Javascript/1-interval/2-scale.js
@@ -1,18 +1,4 @@
-// Introduce el mínimo del intervalo: 4
-// Introduce el máximo del intervalo (superior o igual al mínimo): 10
-// Introduce un factor de escala positivo: 2
-
-// El intervalo [4, 10] con factor de escala 2 es el intervalo [1, 13]
-// Introduce el mínimo del intervalo: 4
-// Introduce el máximo del intervalo (superior o igual al mínimo): 10
-// Introduce un factor de escala positivo: 0.5
-
-// El intervalo [4, 10] con factor de escala 0.5 es el intervalo [5.5, 8.5]
-// Introduce el mínimo del intervalo: 4
-// Introduce el máximo del intervalo (superior o igual al mínimo): 10
-// Introduce un factor de escala positivo: 0
-
-// El intervalo [4, 10] con factor de escala 0 es el intervalo [7, 7]
+//Para admitir factores de escalas decimales, en el archivo console.js del paquete de node, el metodo readnumber usa parseInt y debería usar parseFloat
 
 const { Console } = require("console-mpds");
 const console = new Console();
@@ -22,7 +8,7 @@ const max = console.readNumber(
   `Introduce el máximo del intervalo (superior o igual al mínimo): `
 );
 const scaleFactor = console.readNumber(
-  parseFloat(`Introduce un factor de escala positivo:`)
+  `Introduce un factor de escala positivo:`
 );
 const interval = max - min;
 const centerInterval = interval / 2 + min;
