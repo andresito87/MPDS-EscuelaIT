76ef163aa0cd691bad9fc51b54ea2d1a2936d41f
Andresito <andrespodadera87.gmail.com>
2022-05-05 18:50:00 +0200

    updating 0-1-inverse

--- 15-js-estructurada/0-fraction/0-1-inverse.js
+++ 15-js-estructurada/0-fraction/0-1-inverse.js
@@ -1,31 +1,64 @@
+//Sin Recursividad
+// const { Console } = require("console-mpds");
+// const console = new Console();
+
+// const numerator = console.readNumber(`Introduce el numerador de la fracción: `);
+// const denominator = console.readNumber(`Introduce el denominador de la fracción: `);
+// let greatestCommonDivisor;
+
+// if (numerator > denominator){
+//     let i = denominator;
+//     while(i > 1){
+//          if (denominator%i ===0){
+//             if (numerator%i === 0)
+//                 greatestCommonDivisor = i;        
+//             }
+//         i--;
+//     }
+// }else if (numerator < denominator){
+//     let i = numerator;
+//     while(i > 1){
+//          if (numerator%i ===0){
+//             if (denominator%i === 0)
+//                 greatestCommonDivisor = i;        
+//             }
+//         i--;
+//     }
+// }else {
+//     greatestCommonDivisor = numerator;
+// }
+// console.writeln(`La fracción ${numerator}/${denominator} = ${numerator/greatestCommonDivisor}/\
+// ${denominator/greatestCommonDivisor} invertida es la fracción ${denominator/greatestCommonDivisor}/\
+// ${numerator/greatestCommonDivisor}`);
+//Con Recursividad
 const { Console } = require("console-mpds");
 const console = new Console();
 
-const numerator = console.readNumber(`Introduce el numerador de la fracción: `);
-const denominator = console.readNumber(`Introduce el denominador de la fracción: `);
-let greatestCommonDivisor;
+let numerator = console.readNumber(`Introduce el numerador de la fracción:`);
+let denominator = console.readNumber(`Introduce el denominador de la fracción:`);
+let msg = `La fracción ${numerator}/${denominator}`;
 
-if (numerator > denominator){
-    let i = denominator;
-    while(i > 1){
-         if (denominator%i ===0){
-            if (numerator%i === 0)
-                greatestCommonDivisor = i;        
-            }
-        i--;
+if (numerator != 0 && denominator != 0) {
+    let mcd = 1;
+    if (numerator === denominator) {
+        mcd = numerator;
     }
-}else if (numerator < denominator){
-    let i = numerator;
-    while(i > 1){
-         if (numerator%i ===0){
-            if (denominator%i === 0)
-                greatestCommonDivisor = i;        
-            }
-        i--;
+    let numeratorSimplified = numerator;
+    let denominatorSimplified = denominator;
+    while (numeratorSimplified != denominatorSimplified) {
+        if (numeratorSimplified > denominatorSimplified) {
+            numeratorSimplified -= denominatorSimplified;
+            mcd = numeratorSimplified;
+        }
+        else {
+            denominatorSimplified -= numeratorSimplified;
+            mcd = denominatorSimplified;
+        }
     }
-}else {
-    greatestCommonDivisor = numerator;
+    if (mcd > 1) {
+        numerator /= mcd;
+        denominator /= mcd;
+        msg += ` = ${numerator}/${denominator}`;
+    }
 }
-console.writeln(`La fracción ${numerator}/${denominator} = ${numerator/greatestCommonDivisor}/\
-${denominator/greatestCommonDivisor} invertida es la fracción ${denominator/greatestCommonDivisor}/\
-${numerator/greatestCommonDivisor}`);
+console.writeln(`${msg} invertida es la fracción ${denominator}/${numerator}`);
