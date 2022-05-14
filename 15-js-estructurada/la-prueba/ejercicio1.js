cbb397671adc2e4cd3a90f608c54f737765f66fd
Andresito <andrespodadera87.gmail.com>
2022-05-14 15:23:58 +0200

    updating palindrome

--- 15-js-estructurada/la-prueba/ejercicio1.js
+++ 15-js-estructurada/la-prueba/ejercicio1.js
@@ -5,23 +5,12 @@ let text = console.readString("Dame la cadena, por favor");
 
 let cleanedText = ``;
 for (let i = 0; i < text.length; i++) {
-
-    if (text[i] != " ") {
-        text[i] == "á" ? "a" : "";
-        text[i] == "é" ? "e" : "";
-        text[i] == "í" ? "i" : "";
-        text[i] == "ó" ? "o" : "";
-        text[i] == "ú" ? "u" : "";
-        cleanedText += text[i].toLowerCase();
-    }
+    cleanedText += text[i].toLowerCase();
 }
 
-let i = 0;
-let palindrome;
+let palindrome = true;
 let middlePoint = cleanedText.length / 2;
-do {
-    palindrome = cleanedText[i] === cleanedText[cleanedText.length - 1 - i]
-    i++;
-} while (palindrome && i < middlePoint);
-
+for (let i = 0; i <= middlePoint && palindrome; i++) {
+    palindrome = cleanedText[i] === cleanedText[cleanedText.length - (i + 1)]
+}
 console.writeln(`La cadena introducida ${palindrome ? `si` : `no`} es un palindromo`)
--- 15-js-estructurada/la-prueba/ejercicio4.js
+++ 15-js-estructurada/la-prueba/ejercicio4.js
@@ -5,24 +5,30 @@ let rows = console.readNumber(`Dame las filas`);
 let columns = console.readNumber(`Dame las columnas`);
 let asterisk = [`*`];
 let dot = [`.`];
-//first Line
-let i = 0;
-do {
-    console.write(asterisk);
-    i++;
-} while (i < columns) console.writeln();
-//center lines
-for (let i = 1; i < rows - 1; i++) {
-    console.write(asterisk);
-    for (let j = 1; j < columns - 1; j++) {
-        console.write(dot);
+let lenghtRows = rows * 5;
+let lenghtColumns = columns * 5;
+for (let i = 0; i <= lenghtColumns + 1; i++) {
+    console.write(asterisk)
+}
+console.writeln();
+
+for (i = 0; i < lenghtRows; i++) {
+    for (let j = 5; j < lenghtColumns; j++) {
+        if (j % 5 === 0) {
+            console.write("*");
+        } else if (j % 5 === 3) {
+            console.write("*");
+        } else if (j % 5 === 4) {
+            console.write("")
+        }
+        else {
+            console.write(dot)
+        }
+
     }
-    console.write(asterisk);
     console.writeln();
 }
-//last Line
-i = 0;
-do {
-    console.write(asterisk);
-    i++;
-} while (i < columns);
+
+
+// for (let i = 0; i <= columns; i++) {
+//     console.write(asterisk)
