3cbd4eaa623bb2fa29e6f67a75a9b7e74ac29b95
Andresito <andrespodadera87.gmail.com>
2022-05-24 23:16:28 +0200

    adding mastermind prototype

--- 17-js-orientada-procesos/Mastermind/mastermind.js
+++ 17-js-orientada-procesos/Mastermind/mastermind.js
@@ -1,39 +1,65 @@
 const { Console } = require("console-mpds");
 const console = new Console();
+let randomCombination;
+let secretCombination;
+let gotWinner = false;
+let attemps = 3;
+secretCombination = `rgmc` //Hardcodeada para pruebas
+playGame();
 
-playTicTacToe();
-
-function playTicTacToe() {
+function playGame() {
+    console.writeln(`----- MASTERMIND -----`)
     do {
-        playGame();
-    } while (isResumed());
+        randomCombination = getValidCombination();
+        if (checkCombination()) {
+            gotWinner = true;
+            console.writeln(`¡¡¡You Win,Good Game!!!`)
+        } else if (attemps === 0)
+            console.writeln(`¡¡¡Sorry, you lost!!!`)
+        else {
+            console.writeln(`*********`)
+            console.writeln(`You have ${attemps} remind`);
+            console.writeln(`*********`)
+        }
+    } while (attemps > 0 && !gotWinner);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    function isResumed() {
-        let result;
-        let answer;
-        let error = false;
-        do {
-            answer = console.readString(`Do you want to continue? (y/n): `);
-            result = answer === `y`;
-            error = !result && answer !== `n`;
-            if (error) {
-                console.writeln(`Por favor, responda "y" o "n"`);
-            }
-        } while (error);
-        return result;
+    function checkCombination() {
+        if (randomCombination === secretCombination) {
+            return true;
+        }
+        return false;
     }
-
+    function getValidCombination() {
+        let combination;
+        let foundColor;
+        let isValidCombination;
+        do {
+            combination = console.readString(`Propose a combination:`);
+            if (combination.length != 4) {
+                console.writeln(`Wrong proposed combination length`)
+            }
+            else {
+                let colors = [`r`, `g`, `y`, `b`, `m`, `c`];
+                isValidCombination = true;
+                for (let i = 0; isValidCombination && i <= 3; i++) {
+                    let j;
+                    foundColor = false;
+                    for (j = 0; j <= 5; j++) {
+                        if (combination[i] === colors[j]) {
+                            foundColor = true;
+                        }
+                    }
+                    if (foundColor === false && j === colors.length) {
+                        isValidCombination = false;
+                        console.writeln(`Wrong colors, they must be: rgybmc`);
+                    } else if (foundColor === true && j <= colors.length)
+                        isValidCombination = true;
+                }
+            }
+        } while (!isValidCombination);
+        attemps--;
+        return combination;
+    }
 }
+
